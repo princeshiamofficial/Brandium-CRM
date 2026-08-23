047c696e6d45f1523149fafef433ff322094cada
Prince Shiam <princeshiamofficial@gmail.com>
2026-08-23 22:37:29 +0600

    chore: remove legacy and optional files (cloudflared, supabase, lovable, bunfig)

--- supabase/migrations/20260809090639_248a5dc2-451f-40d4-add5-9f166d1f3da2.sql
+++
@@ -1,4 +0,0 @@
-REVOKE ALL ON FUNCTION public.handle_new_user() FROM PUBLIC;
-REVOKE ALL ON FUNCTION public.update_updated_at_column() FROM PUBLIC;
-REVOKE ALL ON FUNCTION public.has_role(uuid, public.app_role) FROM PUBLIC;
-GRANT EXECUTE ON FUNCTION public.has_role(uuid, public.app_role) TO authenticated;
